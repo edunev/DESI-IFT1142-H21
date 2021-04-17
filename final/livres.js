1d96cc21d4d769849afd064ae6fb112c0bf3fcc2
Andy <andybairoc@gmail.com>
2021-04-17 18:04:17 -0400

    final

---
+++ final/livres.js
@@ -0,0 +1,130 @@
+// Boilerplate
+const tabLivre = [
+  [
+    {
+      "titre": "Asterix en Corse",
+      "auteur": 11,
+      "année": 1973,
+      "pages": 48,
+      "catégorie": "bandes dessinées"
+    },
+    {
+      "titre": "Une autre vie",
+      "auteur": 4,
+      "année": 1988,
+      "pages": 381,
+      "catégorie": "roman"
+    },
+    {
+      "titre": "La belle vie",
+      "auteur": 1,
+      "année": 1989,
+      "pages": 451,
+      "catégorie": "roman"
+    },
+    {
+      "titre": "La maison du guet",
+      "auteur": 6,
+      "année": 1993,
+      "pages": 224,
+      "catégorie": "suspense"
+    },
+    {
+      "titre": "La nuit du renard",
+      "auteur": 6,
+      "année": 1993,
+      "pages": 277,
+      "catégorie": "suspense"
+    },
+    {
+      "titre": "Disparu",
+      "auteur": 4,
+      "année": 1994,
+      "pages": 305,
+      "catégorie": "roman"
+    },
+    {
+      "titre": "La maison des jours heureux",
+      "auteur": 5,
+      "année": 1994,
+      "pages": 496,
+      "catégorie": "roman"
+    },
+    {
+      "titre": "Souviens-toi",
+      "auteur": 6,
+      "année": 1994,
+      "pages": 388,
+      "catégorie": "suspense"
+    },
+    {
+      "titre": "Malveillance",
+      "auteur": 1,
+      "année": 1996,
+      "pages": 391,
+      "catégorie": "roman"
+    },
+    {
+      "titre": "Ce que vivent les roses",
+      "auteur": 6,
+      "année": 1996,
+      "pages": 285,
+      "catégorie": "suspense"
+    },
+    {
+      "titre": "Douce nuit",
+      "auteur": 1,
+      "année": 1996,
+      "pages": 213,
+      "catégorie": "suspense"
+    },
+    {
+      "titre": "Honneur et Courage",
+      "auteur": 4,
+      "année": 1997,
+      "pages": 350,
+      "catégorie": "roman"
+    },
+    {
+      "titre": "La foudre",
+      "auteur": 4,
+      "année": 1997,
+      "pages": 410,
+      "catégorie": "roman"
+    },
+    {
+      "titre": "Ni vue ni connue",
+      "auteur": 6,
+      "année": 1998,
+      "pages": 266,
+      "catégorie": "suspense"
+    },
+    {
+      "titre": "La petite fille qui aimait trop les allumettes",
+      "auteur": 3,
+      "année": 1998,
+      "pages": 180,
+      "catégorie": "roman"
+    },
+    {
+      "titre": "Avant de te dire adieu",
+      "auteur": 6,
+      "année": 2001,
+      "pages": 346,
+      "catégorie": "suspense"
+    },
+    {
+      "titre": "Prisonniers du temps",
+      "auteur": 10,
+      "année": 2001,
+      "pages": 620,
+      "catégorie": "roman"
+    }
+   ]
+]
+
+let questionUn = () => {}
+
+let questionDeux = () => {}
+
+let questionTrois = () => {}
---
+++ final/readme.txt
@@ -0,0 +1,19 @@
+Créer une page HTML  
+    -index.html 
+qui va afficher le titre « Gestion des livres » suivi après de votre nom et prénom. 
+
+Créer 3 boutons dont le texte de chacun sera « Question 1 », « Question 2 » et « Question 3 ». Lorsque vous allez cliquer sur un de ces boutons le code
+correspondant sera exécuté. Ce code se dans une fonction nommée function questionX() {…}, où X correspond au numéro de la question annoncée plus bas. 
+Toutes ces fonctions seront dans le fichier livres.js.
+
+Les questions: 
+
+1. Afficher tous les données du tableau tabLivres, avec un titre « Liste des livres » ainsi que le nom des
+colonnes et après les données.
+
+2. Afficher un champ « input type= » dans lequel vous allez lire le « auteur » (code de l’auteur). Vous allez
+afficher dans le même format que la question 1. 
+
+3. Afficher un champ « input type= » dans lequel vous allez lire la « catégorie ». Vous allez afficher
+seulement les titres de tous les livres de cette catégorie dont « année » (année de publication) est comprise 
+entre 1980 et 1992 inclusivement. 
